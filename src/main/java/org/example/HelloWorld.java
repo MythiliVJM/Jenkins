28ac88125e7c09be2bfd016acd2f5af86651bcbb
Mythili <96191870+MythiliVJM@users.noreply.github.com>
2023-05-17 16:40:37 +0530

    Update HelloWorld.java

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,7 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class HelloWorld {
     public static String sayHello() {
-        return "Helloo, Jenkins!";
+        return "Hello, Jenkins!";
     }
 
     public static void main(String[] args) {
