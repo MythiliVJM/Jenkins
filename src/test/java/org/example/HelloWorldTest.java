c3328396bab49b2c268ece61e8f34a7ed7a6b823
Mythili <96191870+MythiliVJM@users.noreply.github.com>
2023-05-17 16:30:58 +0530

    Update HelloWorldTest.java

--- src/test/java/org/example/HelloWorldTest.java
+++ src/test/java/org/example/HelloWorldTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public  class HelloWorldTest {
     @Test
     public void testSayHello() {
-        String expected = "Hello Jenkins!";
+        String expected = "Hello, Jenkins!";
         String actual = HelloWorld.sayHello();
         assertEquals(expected, actual);
 }}
