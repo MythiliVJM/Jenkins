24d648911b43f86f6440ea167e397b09aff06a82
MythiliVJM <mythilivenugopal2002@gmail.com>
2023-05-17 16:09:27 +0530

    Mythili:Initial Commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class HelloWorld {
+    public static String sayHello() {
+        return "Hello, Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HelloWorld.sayHello());
+    }
+}
---
+++ src/test/java/org/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public  class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        String expected = "Hello, Jenkins!";
+        String actual = HelloWorld.sayHello();
+        assertEquals(expected, actual);
+}}
